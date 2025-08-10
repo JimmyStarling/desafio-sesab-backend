31dfcf1e9e0e9768034c0932fba3136b7121fa75
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-09 23:52:48 -0300

    feat: add user form request

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|string|size:11|unique:users,cpf',
+            'profile_id' => 'required|exists:profiles,id',
+            'addresses' => 'array',
+            'addresses.*' => 'exists:addresses,id'
+        ];
+    }
+}
