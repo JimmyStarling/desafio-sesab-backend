5b39cdc525745693e53ab16df71b97d29d7a3f23
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-10 23:19:36 -0300

    feat(api): add auth routes, with sactum and without(login and register) to crud users, address and profile.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AddressController;
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('profiles', ProfileController::class);
-Route::apiResource('addresses', AddressController::class);
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\AddressController;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'userProfile']);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('profiles', ProfileController::class);
+    Route::apiResource('addresses', AddressController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
