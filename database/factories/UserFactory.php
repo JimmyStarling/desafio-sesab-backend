6c058e5e06f818eb36edd5c4ed0e86f8de7025a3
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-12 21:17:07 -0300

    refactor: minor changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,7 +32,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'profile_id' => null,
-            'cpf' => str_pad(rand(0, 99999999999), 11, '0', STR_PAD_LEFT)
+            'cpf' => str_pad(rand(0, 99999999999), 11, '0', STR_PAD_LEFT),
         ];
     }
 
