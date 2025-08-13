df2cea3222245e643b02f04c6e06b20467e1ee55
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-12 21:14:29 -0300

    feat(exception): add exceptions handler.

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Validation\ValidationException;
+
+class Handler extends ExceptionHandler
+{
+    protected $levels = [
+        // níveis customizados para logging
+    ];
+
+    protected $dontReport = [
+        // exceções que não reportar
+    ];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Validation failed',
+                    'errors' => $exception->errors(),
+                ], 422);
+            }
+        }
+
+        return parent::render($request, $exception);
+    }
+}
