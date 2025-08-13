c93368fa0ffb4f3620987732b15876a61741dec7
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-12 21:16:08 -0300

    feat(factory): add address factory

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    protected $model = Address::class;
+
+    public function definition(): array
+    {
+        return [
+            'street' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'zip' => $this->faker->postcode(),
+        ];
+    }
+}
