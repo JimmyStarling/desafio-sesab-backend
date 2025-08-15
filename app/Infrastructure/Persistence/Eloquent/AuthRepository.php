d7e26232e544efa2309f319485a8be1348350f51
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-15 01:57:58 -0300

    feat(repository): add user service, profile check, two new fields.

--- app/Infrastructure/Persistence/Eloquent/AuthRepository.php
+++ app/Infrastructure/Persistence/Eloquent/AuthRepository.php
@@ -7,11 +7,32 @@ use App\Models\User;
 use App\Models\Address;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use App\Domain\User\Services\UserService;
+use Illuminate\Support\Str;
 
 class AuthRepository implements AuthRepositoryInterface
 {
-    public function register(array $data): User
+
+    public function __construct(UserService $userService)
     {
+        $this->userService = $userService;
+    }
+
+    public function register(bool $is_authenticated, array $data): User
+    {
+
+        // Validate required fields
+        $profileId = $this->userService->checkProfile(
+            auth()->check(),
+            $data['profile_id']
+        );
+        if (!isset($data['name'], $data['email'], $data['cpf'], $data['password'], $data['profile_id'])) {
+            throw new \InvalidArgumentException('Required fields are missing.');
+        }
+        if ($data['profile_id'] === 0) {
+            throw new \Exception('Unauthorized to create user with this profile.', 403);
+        }
+
         // Create user
         $user = User::create([
             'name' => $data['name'],
@@ -19,23 +40,16 @@ class AuthRepository implements AuthRepositoryInterface
             'cpf' => $data['cpf'],
             'password' => Hash::make($data['password']),
             'profile_id' => $data['profile_id'],
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
+        $address = $data['address'] ?? [];
 
-        $addressIds = [];
-
-        foreach ($data['address'] as $addressData) {
-            $address = Address::create($addressData);
-            $addressIds[] = $address->id;
-        }
-
-        if (!empty($addressIds)) {
-            $user->address()->sync($addressIds);
-        } else {
-            throw new \Exception("User must have at least one address.");
-        }
-
-        // Load the address relationship
-        return $user->load('address');
+        // Sync addresses
+        $this->userService->syncAddresses($user, $address);
+        
+        // Load address and profile
+        return $user->load('profile', 'address');
     }
 
     public function login(string $email, string $password): ?User
