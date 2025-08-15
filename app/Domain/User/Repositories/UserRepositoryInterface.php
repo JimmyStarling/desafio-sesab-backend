71ce47108052bab3c3aeb69e42bce488bd4c3df0
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-15 01:38:20 -0300

    refactor(interfaces): change argument names

--- app/Domain/User/Repositories/UserRepositoryInterface.php
+++ app/Domain/User/Repositories/UserRepositoryInterface.php
@@ -8,7 +8,7 @@ use App\Models\User;
 interface UserRepositoryInterface
 {
     public function search(array $filters): LengthAwarePaginator;
-    public function create(array $data, array $addressIds = []): User;
-    public function update(User $user, array $data, array $address = []): User; // $addressIds
+    public function create(bool $is_authenticated, array $data, array $address = []): User; //$addressIds
+    public function update(User $user, array $data, $address = []): User; //$addressIds
     public function delete(User $user): bool;
 }
